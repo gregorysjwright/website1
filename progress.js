14df2288e1a25652bbddefba7bae2b2c608a5f61
root <root@DESKTOP-2NPMBBG>
2023-03-24 02:04:55 +0000

    .

--- progress.js
+++ progress.js
@@ -1,20 +1,31 @@
 fetch('progress-bar.json')
   .then(response => response.json())
   .then(data => {
-  	const progressBar = document.getElementById('progress-bar');
+    const progressBar = document.getElementById('progress-bar');
+    let linksHtml = '';
 
-	data.forEach((item, index) => {
-  	const link = document.createElement('a');
-  	link.href = item.href;
-  	link.classList.add(...item.classes);
- 	 link.id = `link${index + 1}`;
+    data.forEach((item, index) => {
+      const link = document.createElement('a');
+      link.href = item.href;
+      link.classList.add(...item.classes);
+      link.id = `link${index + 1}`;
 
-  	if (item.prize) {
-    		link.innerHTML = '<span class="icon"></span><span class="stage">Prize</span>';
-  	} else 	{
-    		link.innerHTML = '<span class="icon"></span><span class="stage"></span>';
-  	}	
+      if (item.prize) {
+        link.innerHTML = '<span class="icon"></span><span class="stage"></span>';
+        linksHtml += '<br>'; // Start new line for prize links
+      } else {
+        link.innerHTML = '<span class="icon"></span><span class="stage"></span>';
+      }
 
-  progressBar.appendChild(link);
-});
+      linksHtml += link.outerHTML;
+    });
+
+    // Wrap linksHtml in a container div with a fixed width
+    const container = document.createElement('div');
+    container.style.width = '800px'; // Change to desired width
+    container.innerHTML = linksHtml;
+
+    // Insert container into the progress bar element
+    progressBar.appendChild(container);
   });
+
