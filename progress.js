fd478614057023cdff78086a136f1827f0a6bb76
root <root@DESKTOP-2NPMBBG>
2023-03-24 03:14:01 +0000

    ,

--- progress.js
+++ progress.js
@@ -12,7 +12,7 @@ fetch('progress-bar.json')
 
       link.innerHTML = '<span class="icon"></span><span class="stage"></span>';
       if (item.classes.includes("prize")) {
-        links.innerHTML += '<br>'; // Start new line for prize links
+        link.innerHTML += '<br>'; // Start new line for prize links
       }
 
       linksHtml += link.outerHTML;
