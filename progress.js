f99c428d26f9148fc1fe6530577f167404d91007
root <root@DESKTOP-2NPMBBG>
2023-04-08 04:11:54 +0100

    ,

--- progress.js
+++ progress.js
@@ -8,28 +8,33 @@ fetch('progress-bar.json')
 
     // Wrap linksHtml in a container div with a fixed width
     const container = document.createElement('div');
-    container.style.width = '800px'; // Change to desired width
 
     let week = document.createElement("div");
+    let weekLinks = document.createElement("div");
+    weekLinks.classList.add("week-links");
   
     data.forEach((item, index) => {
       const link = document.createElement('a');
       link.href = item.href;
-      link.classList.add(...item.classes, "week-links");
+      link.classList.add(...item.classes);
       link.id = `link${index + 1}`;
       
       link.innerHTML = '<span class="icon"></span><span class="stage"></span>';
 
-      week.appendChild(link);
+      weekLinks.appendChild(link);
       // start new week on prize
       if (item.classes.includes("prize")){
+    let week = document.createElement("div");
         week = document.createElement("div");
+        weekLinks = document.createElement("div");
         const textbox = document.createElement("span");
         textbox.classList.add("week-text")
         week.classList.add("week");
         week.id = `week-${week_index}`;
         textbox.innerHTML=`Week ${week_index}`;
-        week.appendChild(textbox);
+        week.appendChild(textbox);"week-links"
+        weekLinks.classList.add("week-links");
+        week.appendChild(weekLinks);
         container.appendChild(week);
         week_index++;
         
